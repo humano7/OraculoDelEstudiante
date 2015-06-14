f86e6aef62135654282d075d0d7522d3350cc92f
Sergio Correa <sergio.correa.lopez@gmail.com>
2015-06-14 13:39:09 -0300

    Se agrega pagina de inicio

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>.: Oraculo del Estudiante :.</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
+		<script type="text/javascript" src=""></script>
+	</head>
+	<body>
+		<header>
+			<h1>Oraculo del Estudiante</h1>
+	    </header>
+	    <section>
+	    	El oraculo del estudiante, es una simple aplicacion que permite obtener una palabra o concepto, que
+	    	sirva de reflexion, para orientar un cierto momento o estado.
+            <br />
+	    	Historia:<br />
+	    	Este es un simple proyecto que esta basado en unos antiguos documentos budistas, en el cual rondaba 
+	    	al rededor del numero 108.
+            <br />
+	    	Desde que damos nuestros primeros pasos en este mundo, aprendemos dia a dia, somos eternos estudiantes
+	    	de esta vida, unos mas evolucionados que otros, pero en general estamos en una constante de aprendizaje, 
+	    	somos Estudiantes y Maestros a la vez, depende con quien caminemos.
+            <br />
+	    	El 108 es un simbolismo de que existe 108 peldaños para llegar a la evolucion, iluminacion. 108 estados
+	    	que debemos aprender para llegar a ser un ser completo.
+            <br />
+	    	El 108 esta compuesto del numero 1, que simboliza la totalidad, la verdad, el todo, el numero 0, representa el vacio
+	    	la ausencia de algo, falso y el numero 8 si lo ladeamos queda como el simbolo del infinito, lo desconocido
+	    	lo que esta mas alla de nuestra mirada.
+            <br />
+	    	Este proyecto ha quedado como open source y colaborativo.
+
+	    	Version WEB:<br />
+	    	Version Android<br />
+	    	Descarga PDF:<br />
+
+	    </section>
+	    <footer>
+	    	
+	    </footer>
+	</body>
+</html>
+
